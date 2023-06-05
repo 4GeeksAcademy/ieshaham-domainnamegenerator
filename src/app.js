46fe06cb0ea62b1ae8e2c80b4d2f6bc710128456
Ieshaham <131932350+Ieshaham@users.noreply.github.com>
2023-06-05 00:00:18 +0000

    domaingenerator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,34 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  // console.log("Hello Rigo from the console!");
+  let articles = ["the", "an", "some", "another", "any"];
+  let adjectives = ["amazing", "cool", "funny", "thrilling", "awesome"];
+  let nouns = ["websites", "blogs", "hub", "spot", "webapp"];
+  let tlds = [
+    ".com",
+    ".net",
+    ".org",
+    ".ca",
+    ".bs",
+    ".space",
+    ".rock",
+    ".dubai",
+    ".et"
+  ];
+
+  let domainNames = [];
+
+  for (let article of articles) {
+    for (let adjective of adjectives) {
+      for (let noun of nouns) {
+        for (let tld of tlds) {
+          domainNames.push(`${article}${adjective}${noun}${tld}`);
+        }
+      }
+    }
+  }
+  document.body.innerHTML = `<ul>${domainNames
+    .map(domainName => `<li>${domainName}</li>`)
+    .join(" ")}</ul>`;
 };
